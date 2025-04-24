140bf007e3085ebd71c6d3177e4c5813d668c1f8
enkhjin9127 <ka0enkhjin@gmail.com>
2025-04-24 11:16:29 +0800

    BACKEND: fixed friends routes

--- backend/src/resolvers/friends/request-friend.ts
+++ backend/src/resolvers/friends/request-friend.ts
@@ -1,15 +1,26 @@
 import { PrismaClient } from "@prisma/client";
+import { Request, Response } from "express";
 
 const prisma = new PrismaClient();
 
-export const requestFriend = async (userId: string, friendId: string) => {
-  if (userId === friendId) throw new Error("Can't add yourself 💀");
+export const requestFriend = async (req: Request, res: Response) => {
+  try {
+    const userId = req.body.userId;
+    const friendId = req.params.id;
 
-  return await prisma.friendship.create({
-    data: {
-      userId,
-      friendId,
-      status: "pending",
-    },
-  });
+    if (userId === friendId) throw new Error("Can't add yourself 💀");
+
+    const result = await prisma.friendship.create({
+      data: {
+        userId,
+        friendId,
+        status: "pending",
+      },
+    });
+
+    res.status(201).json(result);
+  } catch (err: any) {
+    const errorMessage = err instanceof Error ? err.message : "Unknown error";
+    res.status(400).json({ error: errorMessage });
+  }
 };
