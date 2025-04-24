e3d19d68fedc4444b4eddabfbab8ff07f0b8ded8
IHAW <imalreadylost9@gmail.com>
2025-04-23 19:58:12 -0700

    BACKEND-join-leave-event

--- backend/src/routes/events-routes.ts
+++ backend/src/routes/events-routes.ts
@@ -3,9 +3,13 @@ import express from "express";
 import getEvent from "../resolvers/events/get-event-id";
 import getAllEvents from "../resolvers/events/get-all-events";
 import createEvent from "../resolvers/events/create-event";
+import joinEvent from "../resolvers/events/join-event";
+import leaveEvent from "../resolvers/events/leave-event";
 export const eventsRoute = express.Router();
 
 eventsRoute.post("/", createEvent)
 eventsRoute.get("/:id", getEvent); 
 eventsRoute.get("/", getAllEvents); 
+eventsRoute.post("/:id/join",joinEvent);
+eventsRoute.post("/:id/leave",leaveEvent);
 
