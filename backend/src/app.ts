928caf2f08992bd9276b9ee62d44a5a83d997b85
24LP5396 24LP5396 <24LP5396@G301-CC0208.local>
2025-05-05 11:43:01 +0800

    a

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -38,9 +38,20 @@ const port = process.env.PORT || 8000;
 //   })
 // );
 
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://spottyproject.vercel.app",
+];
+
 app.use(
   cors({
-    origin: true,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, origin);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
