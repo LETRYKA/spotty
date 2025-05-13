8d9c1f6e9639e2ac1b4c21a49d744d9b05a1466b
batorgil123 <imalreadylost9@gmail.com>
2025-05-13 21:39:32 +0800

    orgil-mobile-pro

--- frontend/src/app/profile/_mobile/_components/Friends.tsx
+++ frontend/src/app/profile/_mobile/_components/Friends.tsx
@@ -1,13 +1,14 @@
-'use client'
+"use client";
 
 import { useEffect, useState } from 'react';
 import { ChevronRight, X } from 'lucide-react';
 import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
 import { useRouter } from 'next/navigation';
-import { Button } from "@/components/ui/button";
-import axios from 'axios';
+import { getFriends } from '@/lib/api';
+import { getPendingRequest } from '@/lib/api';
 import { useUser } from "@clerk/nextjs";
-
+import DefaultAvatar from "@/img/default_avatar.png";
+import { set } from 'lodash';
 interface Friend {
   id: string;
   name: string;
@@ -26,26 +27,24 @@ interface Friend {
 
 const Friends = () => {
   const [friends, setFriends] = useState<Friend[]>([]);
+  const [pendingRequests, setPendingRequests] = useState<Friend[]>([]);
   const [loading, setLoading] = useState(true);
   const router = useRouter();
-  const API_URL = process.env.NEXT_PUBLIC_API_URL;
   const { user } = useUser();
   const userId = user?.id;
 
   useEffect(() => {
     if (!userId) return;
-
     const fetchFriends = async () => {
-      try {
-        const res = await axios.get(`${API_URL}/api/friends/${userId}`);
-        setFriends(res.data);
-      } catch (error) {
-        console.error('Error fetching friends:', error);
-      } finally {
-        setLoading(false);
-      }
+      const data = await getFriends(userId);
+      const data1 = await getPendingRequest(userId);
+      setPendingRequests(data1);
+      setFriends(data);
+      setLoading(false);
+      console.log("data1",data1);
+          console.log("data",data);
+      
     };
-
     fetchFriends();
   }, [userId]);
 
@@ -54,36 +53,41 @@ const Friends = () => {
   };
 
   if (loading) {
-    return <p className="text-white text-sm">Loading friends...</p>;
+    return <p className="text-white text-sm">Loading...</p>;
   }
 
   return (
     <div className="flex flex-col gap-6">
-      <div
-        className="w-full flex justify-between items-center cursor-pointer"
-        onClick={handleClick}
-      >
-        <div className="flex items-center gap-3">
-          <Avatar className="relative h-[60px] w-[60px]">
-            <AvatarImage
-              className="rounded-full object-cover w-full h-full"
-              src={friends[0]?.avatarImage || '/default-avatar.png'}
-              alt={friends[0]?.name || 'User'}
-            />
-          </Avatar>
-          <div>
-            <p className="text-xs text-white/70">
-              {friends[0]?.name} + {friends.length - 1} others
-            </p>
-          </div>
-        </div>
-        <ChevronRight className="text-white" />
-      </div>
+      {pendingRequests?.length > 0 && (
+        <div
+          className="w-full flex justify-between items-center cursor-pointer"
+          onClick={handleClick}
+        >
+          <div className="flex items-center gap-3">
+            <Avatar className="relative h-[60px] w-[60px]">
+              <AvatarImage
+                className="rounded-full object-cover w-full h-full"
+                src={pendingRequests[0]?.avatarImage || DefaultAvatar.src}
+                alt={pendingRequests[0]?.name || 'User'}
+              />
+            </Avatar>
+            {pendingRequests.length > 1 && (
+              <div>
+                <p className="text-xs text-white/70">
+                  {pendingRequests[0]?.name} + {pendingRequests.length - 1} others
+                </p>
+              </div>
+            )}
 
-      <h1 className="text-white text-xl font-bold">All Friends</h1>
+          </div>
+          <ChevronRight className="text-white" />
+        </div>)}
+
+
+      <h1 className="text-white text-xl font-bold">Найзууд</h1>
 
       {friends.length === 0 ? (
-        <p className="text-white/60 text-sm">No friends found.</p>
+        <p className="text-white/60 text-sm">Найз байхгүй.</p>
       ) : (
         <div className="flex flex-col gap-4">
           {friends.map((friend) => (
@@ -92,7 +96,7 @@ const Friends = () => {
                 <Avatar>
                   <AvatarImage
                     className="rounded-full object-cover w-[50px] h-[50px]"
-                    src={friend.avatarImage || '/default-avatar.png'}
+                    src={friend.avatarImage || DefaultAvatar.src}
                     alt={friend.name}
                   />
                 </Avatar>
