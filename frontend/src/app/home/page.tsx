929987a0b7b5301f94b4490d493ea57011ba0ec5
LETRYKA <helloaryka@gmail.com>
2025-05-14 10:55:19 +0800

    a

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -7,7 +7,8 @@ import {
   SignUpButton,
   UserButton,
 } from "@clerk/nextjs";
-import { DockComp } from "./_components/Dock";
+//a
+import { DockComp } from "../../components/Dock";
 import { CardCarousel } from "./_components/Carousel";
 import { InteractiveHoverButton } from "@/components/magicui/interactive-hover-button";
 import SplitText from "@/components/SplitText";
