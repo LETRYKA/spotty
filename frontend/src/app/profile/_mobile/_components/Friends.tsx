fe50cb99ed2dad76ba592a2d77a28da904bbeedf
IHAW <imalreadylost9@gmail.com>
2025-05-15 18:35:11 -0700

    f

--- frontend/src/app/profile/_mobile/_components/Friends.tsx
+++ frontend/src/app/profile/_mobile/_components/Friends.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import { useEffect, useState } from 'react';
-import { ChevronRight, X } from 'lucide-react';
+import { useEffect, useState } from "react";
+import { ChevronRight, X } from "lucide-react";
 import { Avatar, AvatarImage } from "@radix-ui/react-avatar";
-import { useRouter } from 'next/navigation';
-import { getFriends } from '@/lib/api';
-import { getPendingRequest } from '@/lib/api';
+import { useRouter } from "next/navigation";
+import { getFriends } from "@/lib/api";
+import { getPendingRequest } from "@/lib/api";
 import { useUser } from "@clerk/nextjs";
 import DefaultAvatar from "@/img/default_avatar.png";
-import { set } from 'lodash';
+import { Button } from "@/components/ui/button";
+import UnfAlert from "./../../_web/_components/unfAlert";
 interface Friend {
   id: string;
   name: string;
@@ -41,15 +42,14 @@ const Friends = () => {
       setPendingRequests(data1);
       setFriends(data);
       setLoading(false);
-      console.log("data1",data1);
-          console.log("data",data);
-      
+      console.log("data1", data1);
+      console.log("data", data);
     };
     fetchFriends();
   }, [userId]);
 
   const handleClick = () => {
-    router.push('/requests');
+    router.push("/requests");
   };
 
   if (loading) {
@@ -68,50 +68,50 @@ const Friends = () => {
               <AvatarImage
                 className="rounded-full object-cover w-full h-full"
                 src={pendingRequests[0]?.avatarImage || DefaultAvatar.src}
-                alt={pendingRequests[0]?.name || 'User'}
+                alt={pendingRequests[0]?.name || "User"}
               />
             </Avatar>
-            {pendingRequests.length===1 && (<p className='text-white/70'>{pendingRequests[0]?.name}</p>)}
+            {pendingRequests.length === 1 && (
+              <p className="text-white/70">{pendingRequests[0]?.name}</p>
+            )}
             {pendingRequests.length > 1 && (
               <div>
                 <p className="text-xs text-white/70">
-                  {pendingRequests[0]?.name} + {pendingRequests.length - 1} others
+                  {pendingRequests[0]?.name} + {pendingRequests.length - 1}{" "}
+                  others
                 </p>
               </div>
             )}
-
           </div>
           <ChevronRight className="text-white" />
-        </div>)}
-
+        </div>
+      )}
 
       <h1 className="text-white text-xl font-bold">Найзууд</h1>
 
-      {friends.length === 0 ? (
-        <p className="text-white/60 text-sm">Найз байхгүй.</p>
-      ) : (
-        <div className="flex flex-col gap-4">
-          {friends.map((friend) => (
-            <div onClick={()=> router.push(`/${friend.name}`)} key={friend.id} className="flex justify-between items-center">
-              <div className="flex gap-3 items-center">
-                <Avatar>
-                  <AvatarImage
-                    className="rounded-full object-cover w-[50px] h-[50px]"
-                    src={friend.avatarImage || DefaultAvatar.src}
-                    alt={friend.name}
-                  />
-                </Avatar>
-                <div className="flex flex-col">
-                  <h1 className="text-white text-sm">{friend.name}</h1>
-                  {friend.moodStatus && (
-                    <p className="text-white/40 text-xs">{friend.moodStatus}</p>
-                  )}
-                </div>
-              </div>
+      {friends.map((friend) => (
+        <div key={friend.id} className="flex justify-between items-center">
+          <div
+            onClick={() => router.push(`/${friend.name}`)}
+            className="flex gap-3 items-center"
+          >
+            <Avatar>
+              <AvatarImage
+                className="rounded-full object-cover w-[50px] h-[50px]"
+                src={friend.avatarImage || DefaultAvatar.src}
+                alt={friend.name}
+              />
+            </Avatar>
+            <div className="flex flex-col">
+              <h1 className="text-white text-sm">{friend.name}</h1>
+              {friend.moodStatus && (
+                <p className="text-white/40 text-xs">{friend.moodStatus}</p>
+              )}
             </div>
-          ))}
+          </div>
+          <UnfAlert userId={userId as string} friendId={friend.id as string} />
         </div>
-      )}
+      ))}
     </div>
   );
 };
