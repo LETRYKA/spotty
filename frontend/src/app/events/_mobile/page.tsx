691b650035801b10e29c0f0248ad2ec1aa3d1b5c
24LP2782 24LP2782 <24LP2782@G304-CC0223-2.local>
2025-06-04 10:29:16 +0800

    Events Mobile Fix

--- frontend/src/app/events/_mobile/page.tsx
+++ frontend/src/app/events/_mobile/page.tsx
@@ -1,40 +1,31 @@
 "use client";
+
 import { RainbowButton } from "@/components/magicui/rainbow-button";
 import EventsCarousel from "./_components/EventsCarousel";
 import UpcomingHeader from "./_components/UpcomingHeader";
 import { useRouter } from "next/navigation";
- 
- 
 import { useState } from "react";
- 
-import { House } from 'lucide-react';
-import { MapPlus } from 'lucide-react';
+import { MapPlus } from "lucide-react";
 
- 
 const MobileAllEvents = () => {
   const router = useRouter();
   const [status, setStatus] = useState("UPCOMING");
-  console.log("status:", status);
-  return (
- 
-    <div className="w-full h-full flex flex-col gap-5">
-      <UpcomingHeader status={status} onStatusChange={setStatus} />
 
- 
-      {/* <div className="h-[700px]"> */}
+  return (
+    <div className="relative w-full min-h-screen flex flex-col gap-4 pb-32 pt-10">
+      <UpcomingHeader status={status} onStatusChange={setStatus} />
       <EventsCarousel status={status} />
-      {/* </div> */}
-      <div className="w-full flex justify-center items-center">
+      
+      <div className="fixed bottom-12 left-1/2 -translate-x-1/2 z-50">
         <RainbowButton
- 
-            onClick={() => router.back()}
-            className="h-18 w-18 aspect-square rounded-full text-3xl font-bold pointer-events-auto "
-          >
-            <MapPlus className="w-[25px] h-[25px]" />
- 
+          onClick={() => router.back()}
+          className="h-16 w-16 rounded-full flex items-center justify-center shadow-lg bg-white"
+        >
+          <MapPlus className="w-[24px] h-[24px]" />
         </RainbowButton>
       </div>
     </div>
   );
 };
+
 export default MobileAllEvents;
